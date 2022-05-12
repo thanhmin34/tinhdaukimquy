b1b8e06fe2a6f54a4121c1e66859d4dae8b6c362
unknown <thanhmin34@gmai.com>
2022-05-12 11:33:36 +0700

    update

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+    colors: {
+      primary: "#1fb6ff",
+      secondary: "#7e5bef",
+      w: "#fff",
+      b: "#000",
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+  },
+  plugins: [],
+};
