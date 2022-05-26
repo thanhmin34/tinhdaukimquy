ea268c2b524c2f2528af7be86a6f02207eb1eed5
hoang <thanhmin34@gmai.com>
2022-05-26 22:22:53 +0700

    hoang

---
+++ build/static/js/856.bebe0894.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunktinhdaukimquy=self.webpackChunktinhdaukimquy||[]).push([[856],{6856:function(t,n,a){a.d(n,{ZkW:function(){return u}});var h=a(9983);function u(t){return(0,h.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6v12zM19 4h-3.5l-1-1h-5l-1 1H5v2h14V4z"}}]})(t)}}}]);
+//# sourceMappingURL=856.bebe0894.chunk.js.map
